0302c8c445960532b80fbc4510f806f4b1238016
nima <nimiology@gmial.com>
2021-06-15 00:49:11 +0430

    Autofill for SLUG added

--- Songs/admin.py
+++ Songs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SingleTrack
+
+class SingleTrackAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'Slug': ['Title','Artist']}
 
 # Register your models here.
+admin.site.register(SingleTrack,SingleTrackAdmin)
--- Songs/models.py
+++ Songs/models.py
@@ -6,16 +6,17 @@ def Validator(value):
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.mp3','.wave']
     if not ext.lower() in valid_extensions:
-        raise ValidationError('Unsupported file extension.')
+        raise ValidationError('song should be MP3 or WAVE')
 
 
 # Create your models here.
+#TODO: Cover va song be soorat unique tolid beshan
 class SingleTrack(models.Model):
     Title = models.CharField(max_length=1000)
     Artist = models.CharField(max_length=1000)
     Slug = models.SlugField(unique=True)
-    Cover = models.ImageField(upload_to='',unique=True)
-    Song = models.FileField(validators=[Validator])
+    Cover = models.ImageField(upload_to=f'singleTrack/cover')
+    Song = models.FileField(validators=[Validator],upload_to='singleTrack/song')
 
 
     def __str__(self):
