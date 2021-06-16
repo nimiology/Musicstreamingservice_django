1b82639403d5db0fc2913d9eb81e5b8b1f2fd76a
nima <nimiology@gmial.com>
2021-06-16 14:36:23 +0430

    Album ADDED

--- Songs/admin.py
+++ Songs/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import SingleTrack
+from .models import SingleTrack,Album
 
 class SingleTrackAdmin(admin.ModelAdmin):
     prepopulated_fields = {'Slug': ['Title','Artist']}
 
+class AlbumAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'Slug': ['Title','Artist']}
 # Register your models here.
 admin.site.register(SingleTrack,SingleTrackAdmin)
+admin.site.register(Album,AlbumAdmin)
