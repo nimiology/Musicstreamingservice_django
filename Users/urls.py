ef41d3c76b8b4b557679a13313c1cb5ba2b6712e
nima <nimiologyy@gmail.com>
2022-01-01 18:07:31 +0330

    login register

--- Users/urls.py
+++ Users/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
 
 app_name = 'Users'
-urlpatterns = []
+urlpatterns = [
+    url('auth/', include('djoser.urls')),
+    url('auth/', include('djoser.urls.jwt'))
+]
